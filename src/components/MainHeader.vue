097673b783cfbfc4961ae78297fd2e470d5babaa
jalebi <jalebi@sabacell.net>
2022-08-28 02:28:58 +0430

    feat: add styles + logo + components structure

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <img alt="Eurowings logo" src="../assets/images/logo.png" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+};
+</script>
