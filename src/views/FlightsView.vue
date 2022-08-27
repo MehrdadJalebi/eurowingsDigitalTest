6a52d4a56954d56fbc01a893b73fb2467e92ba0c
jalebi <jalebi@sabacell.net>
2022-08-28 02:40:17 +0430

    feat: add bg image

--- src/views/FlightsView.vue
+++ src/views/FlightsView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="home"></div>
+  <div class="flights">
+    <img
+      class="main-image"
+      src="../assets/images/mainPageImage.jpeg"
+      alt="main page image"
+    />
+  </div>
 </template>
 
 <script>
